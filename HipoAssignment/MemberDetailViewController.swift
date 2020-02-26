525fcc517a0eb42b0bc80b6c68e4e2bd92d219c6
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-02-26 18:01:24 +0300

    Here we go!

---
+++ HipoAssignment/MemberDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemberDetailViewController.swift
+//  HipoAssignment
+//
+//  Created by Ali Emre Değirmenci on 26.02.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import UIKit
+
+class MemberDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HipoAssignment/MembersViewController.swift
+++ HipoAssignment/MembersViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MembersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
