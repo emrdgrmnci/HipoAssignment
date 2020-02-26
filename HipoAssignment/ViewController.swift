f07ed602f01caa4bc512b190705885f4052663c0
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-02-26 17:12:50 +0300

    Initial Commit

---
+++ HipoAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HipoAssignment
+//
+//  Created by Ali Emre Değirmenci on 26.02.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
