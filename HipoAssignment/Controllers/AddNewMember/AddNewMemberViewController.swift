163ceabb68e43260a0e7a6b1fcd433fb1a55578d
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-04-22 16:13:56 +0300

    Partial long functions in AddNewMemberVC

--- HipoAssignment/Controllers/AddNewMember/AddNewMemberViewController.swift
+++ HipoAssignment/Controllers/AddNewMember/AddNewMemberViewController.swift
@@ -89,12 +89,69 @@ class AddNewMemberViewController: UIViewController, UITextFieldDelegate {
         else {
             return false
         }
-        }
+    }
+
+    public static func Init<Type>(value : Type, block: (_ object: Type) -> Void) -> Type {
+        block(value)
+        return value
+    }
 
     //MARK: - Setup View
     func setupView() {
-        view.addSubview(scrollView)
+        viewAddSubView()
         scrollView.backgroundColor = .systemBackground
+        [scrollView, saveButton].forEach { $0.translatesAutoresizingMaskIntoConstraints = false}
+
+        labelUI()
+        labelConstraints()
+        textFieldUI()
+        textFieldConstraints()
+        scrollViewConstraints()
+
+        scrollViewAddSubView()
+
+        nameLabel.text = "Name:"
+        nameLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20).isActive = true
+
+        nameTextField.placeholder = "Please enter your name"
+        nameTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 40).isActive = true
+
+        positionLabel.text = "Position:"
+        positionLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 100).isActive = true
+
+        positionTextField.placeholder = "Please enter your position"
+        positionTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 120).isActive = true
+
+        ageLabel.text = "Age:"
+        ageLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 180).isActive = true
+
+        ageTextField.placeholder = "Please enter your age"
+        ageTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 200).isActive = true
+
+        locationLabel.text = "Location:"
+        locationLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 260).isActive = true
+
+        locationTextField.placeholder = "Please enter your location"
+        locationTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 280).isActive = true
+
+        yearsInHipoLabel.text = "Number of years in Hipo:"
+        yearsInHipoLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 340).isActive = true
+
+        yearsInHipoTextField.placeholder = "Please enter how many years you worked in Hipo"
+        yearsInHipoTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 360).isActive = true
+
+        githubLabel.text = "Github:"
+        githubLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 420).isActive = true
+
+        githubTextField.placeholder = "Please enter your Github username"
+        githubTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 440).isActive = true
+
+        saveButtonConstraints()
+    }
+
+    //MARK: - viewAddSubView()
+    func viewAddSubView() {
+        view.addSubview(scrollView)
         view.addSubview(nameLabel)
         view.addSubview(nameTextField)
         view.addSubview(positionLabel)
@@ -108,170 +165,78 @@ class AddNewMemberViewController: UIViewController, UITextFieldDelegate {
         view.addSubview(githubLabel)
         view.addSubview(githubTextField)
         view.addSubview(saveButton)
+    }
 
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
+    //MARK: - scrollViewAddSubView
+    func scrollViewAddSubView() {
+        scrollView.addSubview(nameLabel)
+        scrollView.addSubview(nameTextField)
+        scrollView.addSubview(positionLabel)
+        scrollView.addSubview(positionTextField)
+        scrollView.addSubview(ageLabel)
+        scrollView.addSubview(ageTextField)
+        scrollView.addSubview(locationLabel)
+        scrollView.addSubview(locationTextField)
+        scrollView.addSubview(yearsInHipoLabel)
+        scrollView.addSubview(yearsInHipoTextField)
+        scrollView.addSubview(githubLabel)
+        scrollView.addSubview(githubTextField)
+        scrollView.addSubview(saveButton)
+    }
+
+    //MARK: - labelUI
+    func labelUI() {
+        [nameLabel, positionLabel, ageLabel, locationLabel, yearsInHipoLabel, githubLabel].forEach {
+            $0.font = $0.font.withSize(13)
+            $0.textColor = .black
+        }
+    }
+
+    //MARK: - labelConstraints
+    func labelConstraints() {
+        [nameLabel, positionLabel, ageLabel, locationLabel, yearsInHipoLabel, githubLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30).isActive = true
+        }
+    }
+
+    //MARK: - textFieldUI
+    func textFieldUI() {
+        [nameTextField, positionTextField, ageTextField, locationTextField, yearsInHipoTextField, githubTextField].forEach {
+            $0.font = UIFont.systemFont(ofSize: 15)
+            $0.backgroundColor = .systemGray6
+            $0.borderStyle = UITextField.BorderStyle.roundedRect
+            $0.autocorrectionType = UITextAutocorrectionType.no
+            $0.keyboardType = UIKeyboardType.default
+            $0.returnKeyType = UIReturnKeyType.done
+            $0.clearButtonMode = UITextField.ViewMode.whileEditing
+            $0.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        }
+    }
+
+    //MARK: - textFieldConstraints
+    func textFieldConstraints() {
+        [nameTextField, positionTextField, ageTextField, locationTextField, yearsInHipoTextField, githubTextField].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+            $0.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
+            $0.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+        }
+    }
+
+    //MARK: - scrollViewConstraints
+    func scrollViewConstraints() {
         scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
 
-        scrollView.addSubview(nameLabel)
-        nameLabel.text = "Name:"
-        nameLabel.font = nameLabel.font.withSize(13)
-        nameLabel.textColor = .black
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        nameLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20).isActive = true
-        nameLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30).isActive = true
-
-        scrollView.addSubview(nameTextField)
-        nameTextField.placeholder = "Please enter your name"
-        nameTextField.font = UIFont.systemFont(ofSize: 15)
-        nameTextField.backgroundColor = .systemGray6
-        nameTextField.borderStyle = UITextField.BorderStyle.roundedRect
-        nameTextField.autocorrectionType = UITextAutocorrectionType.no
-        nameTextField.keyboardType = UIKeyboardType.default
-        nameTextField.returnKeyType = UIReturnKeyType.done
-        nameTextField.clearButtonMode = UITextField.ViewMode.whileEditing
-        nameTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        nameTextField.translatesAutoresizingMaskIntoConstraints = false
-        nameTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        nameTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 40).isActive = true
-        nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
-        nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
-
-        scrollView.addSubview(positionLabel)
-        positionLabel.text = "Position:"
-        positionLabel.font = positionLabel.font.withSize(13)
-        positionLabel.textColor = .black
-        positionLabel.translatesAutoresizingMaskIntoConstraints = false
-        positionLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        positionLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 100).isActive = true
-        positionLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30).isActive = true
-
-        scrollView.addSubview(positionTextField)
-        positionTextField.placeholder = "Please enter your position"
-        positionTextField.font = UIFont.systemFont(ofSize: 15)
-        positionTextField.backgroundColor = .systemGray6
-        positionTextField.borderStyle = UITextField.BorderStyle.roundedRect
-        positionTextField.autocorrectionType = UITextAutocorrectionType.no
-        positionTextField.keyboardType = UIKeyboardType.default
-        positionTextField.returnKeyType = UIReturnKeyType.done
-        positionTextField.clearButtonMode = UITextField.ViewMode.whileEditing
-        positionTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        positionTextField.translatesAutoresizingMaskIntoConstraints = false
-        positionTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        positionTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 120).isActive = true
-        positionTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
-        positionTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
-
-        scrollView.addSubview(ageLabel)
-        ageLabel.text = "Age:"
-        ageLabel.font = ageLabel.font.withSize(13)
-        ageLabel.textColor = .black
-        ageLabel.translatesAutoresizingMaskIntoConstraints = false
-        ageLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        ageLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 180).isActive = true
-        ageLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30).isActive = true
-
-        scrollView.addSubview(ageTextField)
-        ageTextField.placeholder = "Please enter your age"
-        ageTextField.font = UIFont.systemFont(ofSize: 15)
-        ageTextField.backgroundColor = .systemGray6
-        ageTextField.borderStyle = UITextField.BorderStyle.roundedRect
-        ageTextField.autocorrectionType = UITextAutocorrectionType.no
-        ageTextField.keyboardType = UIKeyboardType.default
-        ageTextField.returnKeyType = UIReturnKeyType.done
-        ageTextField.clearButtonMode = UITextField.ViewMode.whileEditing
-        ageTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        ageTextField.translatesAutoresizingMaskIntoConstraints = false
-        ageTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        ageTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 200).isActive = true
-        ageTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
-        ageTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
-
-        scrollView.addSubview(locationLabel)
-        locationLabel.text = "Location:"
-        locationLabel.font = locationLabel.font.withSize(13)
-        locationLabel.textColor = .black
-        locationLabel.translatesAutoresizingMaskIntoConstraints = false
-        locationLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        locationLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 260).isActive = true
-        locationLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30).isActive = true
-
-        scrollView.addSubview(locationTextField)
-        locationTextField.placeholder = "Please enter your location"
-        locationTextField.font = UIFont.systemFont(ofSize: 15)
-        locationTextField.backgroundColor = .systemGray6
-        locationTextField.borderStyle = UITextField.BorderStyle.roundedRect
-        locationTextField.autocorrectionType = UITextAutocorrectionType.no
-        locationTextField.keyboardType = UIKeyboardType.default
-        locationTextField.returnKeyType = UIReturnKeyType.done
-        locationTextField.clearButtonMode = UITextField.ViewMode.whileEditing
-        locationTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        locationTextField.translatesAutoresizingMaskIntoConstraints = false
-        locationTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        locationTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 280).isActive = true
-        locationTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
-        locationTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
-
-        scrollView.addSubview(yearsInHipoLabel)
-        yearsInHipoLabel.text = "Number of years in Hipo:"
-        yearsInHipoLabel.font = yearsInHipoLabel.font.withSize(13)
-        yearsInHipoLabel.textColor = .black
-        yearsInHipoLabel.translatesAutoresizingMaskIntoConstraints = false
-        yearsInHipoLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        yearsInHipoLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 340).isActive = true
-        yearsInHipoLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30).isActive = true
-
-        scrollView.addSubview(yearsInHipoTextField)
-        yearsInHipoTextField.placeholder = "Please enter how many years you worked in Hipo"
-        yearsInHipoTextField.font = UIFont.systemFont(ofSize: 15)
-        yearsInHipoTextField.backgroundColor = .systemGray6
-        yearsInHipoTextField.borderStyle = UITextField.BorderStyle.roundedRect
-        yearsInHipoTextField.autocorrectionType = UITextAutocorrectionType.no
-        yearsInHipoTextField.keyboardType = UIKeyboardType.default
-        yearsInHipoTextField.returnKeyType = UIReturnKeyType.done
-        yearsInHipoTextField.clearButtonMode = UITextField.ViewMode.whileEditing
-        yearsInHipoTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        yearsInHipoTextField.translatesAutoresizingMaskIntoConstraints = false
-        yearsInHipoTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        yearsInHipoTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 360).isActive = true
-        yearsInHipoTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
-        yearsInHipoTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
-
-        scrollView.addSubview(githubLabel)
-        githubLabel.text = "Github:"
-        githubLabel.font = githubLabel.font.withSize(13)
-        githubLabel.textColor = .black
-        githubLabel.translatesAutoresizingMaskIntoConstraints = false
-        githubLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        githubLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 420).isActive = true
-        githubLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 30).isActive = true
-
-        scrollView.addSubview(githubTextField)
-        githubTextField.placeholder = "Please enter your Github username"
-        githubTextField.font = UIFont.systemFont(ofSize: 15)
-        githubTextField.backgroundColor = .systemGray6
-        githubTextField.borderStyle = UITextField.BorderStyle.roundedRect
-        githubTextField.autocorrectionType = UITextAutocorrectionType.no
-        githubTextField.keyboardType = UIKeyboardType.default
-        githubTextField.returnKeyType = UIReturnKeyType.done
-        githubTextField.clearButtonMode = UITextField.ViewMode.whileEditing
-        githubTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        githubTextField.translatesAutoresizingMaskIntoConstraints = false
-        githubTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        githubTextField.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 440).isActive = true
-        githubTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25).isActive = true
-        githubTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
-
-        scrollView.addSubview(saveButton)
-        //MARK: - add new member button layouts
+    //MARK: - add new member button layouts
+    func saveButtonConstraints() {
         saveButton.setTitle("SAVE", for: .normal)
         saveButton.backgroundColor = UIColor(red:0.18, green:0.73, blue:0.31, alpha:1.0)
-        saveButton.translatesAutoresizingMaskIntoConstraints = false
-        saveButton.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        //        saveButton.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20).isActive = true
         saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
         saveButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 560).isActive = true
         saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
@@ -345,6 +310,5 @@ class AddNewMemberViewController: UIViewController, UITextFieldDelegate {
             self.showAlert(withTitle: "Error", withMessage: "New member does not saved successfully!")
             print(error.localizedDescription)
         }
-
     }
 }
